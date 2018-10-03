7b4234c5e04f6a597e285bc0d7d068b23f4be9db
James Taber <JATaber@fullsail.edu>
2018-10-03 01:34:45 -0500

    captured required info to be displayed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="home">
-    <h1>Welcome</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <button @click="getNextCharacter">NEXT</button>
-    <button @click="getPrevCharacter">Previous</button>
+    <h1>Star Wars Character Info</h1>
+    <h2>{{character.name}}</h2>
+    <h3>Birth year: {{character.birth_year}}</h3>
+    <button v-show="characterNumber > 1" @click="getPrevCharacter">Previous</button>
+    <button v-show="characterNumber < 87" @click="getNextCharacter">NEXT</button>
   </div>
 </template>
 
@@ -20,43 +17,90 @@ export default {
     return {
       characterNumber: null,
       character: null,
+      homeplanet: null,
+      starship: [],
+      shipInfo: [],
+      planetInfo: [],
       errors: []
     }
   },
   mounted () {
     this.randomNumber()
   },
+  components: {
+    'homeworlds':{
+      template: ''
+    },
+    'spaceship':{
+      template: ''
+    }
+  },
   methods: {
     randomNumber () {
       this.characterNumber = Math.floor(Math.random() * 87) + 1;
       console.log(this.characterNumber);
-      this.getCharacter();
+      this.getCharacterInfo();
     },
-    async getCharacter () {
+    async getCharacterInfo () {
+      //clear the starship array
+      this.starship = [];
+
+      //call API for character info with random generated character id number
       let url = 'https://swapi.co/api/people/'+this.characterNumber.toString();
       console.log(url);
       const response = await axios.get(url);
       this.character = response.data;
-      console.log(this.character.name);
-      /*axios.get(url)
+
+      //put the homeplanet info into its variable
+      this.homeplanet = this.character.homeworld;
+
+      //check if the homeplanet variable is still null if not call the getPlanet method
+      if(this.homeplanet !== null){
+        this.getPlanet();
+      }
+
+      //check to see is character is tied to any starships and push info into the starship array
+      if(this.character.starships.length > 0){
+        this.character.starships.forEach((starship) => {
+          this.starship.push(starship);
+        });
+      }
+
+      //check to see anything was added to the starship array
+      if(this.starship.length > 0){
+        //if so do an API call for the starships and push into the shipInfo array
+        this.getShip();
+        }
+
+      console.log(this.character);
+    },
+    getShip () {
+      this.shipInfo = [];
+      this.starship.forEach((ship) => {
+        axios.get(ship)
+          .then(response => {
+            this.shipInfo.push(response.data);
+            console.log(this.shipInfo);
+          })
+      })
+    },
+    getPlanet () {
+      this.planetInfo = [];
+      axios.get(this.homeplanet)
         .then(response => {
-          console.log(response.data)
-          this.character = response.data;
+          this.planetInfo.push(response.data);
+          console.log(this.planetInfo[0]);
         })
-        .catch(e => {
-          this.errors.push(e)
-        });*/
-      //console.log(this.character);
     },
     getNextCharacter () {
       this.characterNumber++;
       console.log(this.characterNumber);
-      this.getCharacter();
+      this.getCharacterInfo();
     },
     getPrevCharacter () {
       this.characterNumber--;
       console.log(this.characterNumber);
-      this.getCharacter();
+      this.getCharacterInfo();
     }
   }
 }
